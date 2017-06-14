a1a28f5d17c0d30bfba35a215c2c419b37626b11
Nathan Griffith <nathan.griffith@betterment.com>
2017-06-14 17:37:17 -0400

    Implement ReportDiff

--- lib/simplecov/diff.rb
+++ lib/simplecov/diff.rb
@@ -2,6 +2,7 @@ require 'active_support/core_ext/hash/keys'
 require 'simplecov/diff/version'
 require 'simplecov/diff/report'
 require 'simplecov/diff/file'
+require 'simplecov/diff/report_diff'
 
 module SimpleCov
   module Diff
---
+++ lib/simplecov/diff/report_diff.rb
@@ -0,0 +1,38 @@
+module SimpleCov
+  module Diff
+    class ReportDiff
+      def initialize(l_report, r_report)
+        self.left = l_report
+        self.right = r_report
+      end
+
+      def files
+        files_by_filename.values
+      end
+
+    private
+
+      def files_by_filename
+        filenames.each_with_object({}) do |name, hsh|
+          hsh[name] = OpenStruct.new filename: name,
+                                     left: left_files_by_filename[name]&.first,
+                                     right: right_files_by_filename[name]&.first
+        end
+      end
+
+      def left_files_by_filename
+        left.files.group_by(&:filename)
+      end
+
+      def right_files_by_filename
+        right.files.group_by(&:filename)
+      end
+
+      def filenames
+        left.files.map(&:filename) | right.files.map(&:filename)
+      end
+
+      attr_accessor :left, :right
+    end
+  end
+end
