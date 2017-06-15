87045b7fca00f1bce5e1e84a70d8ae6362d850ae
Nathan Griffith <nathan.griffith@betterment.com>
2017-06-15 13:16:30 -0400

    Hold onto fewer vars during iteration

--- lib/undercover/git_diff.rb
+++ lib/undercover/git_diff.rb
@@ -11,38 +11,28 @@ module Undercover
     attr_writer :files
 
     def parse_file_diffs(string) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/LineLength
-      files = []
-      current_file = nil
-      current_chunk = nil
       old_line = nil
       new_line = nil
-      string.each_line do |line|
+      string.each_line.each_with_object([]) do |line, files|
         if line.start_with?('+++')
-          current_file[:new_file] = line[4..-1]
+          files.last[:new_file] = line[4..-1]
         elsif line.start_with?('---')
-          current_file[:chunks] << current_chunk if current_chunk
-          files << current_file if current_file
-          current_file = { old_file: line[4..-1], chunks: [] }
-          current_chunk = nil
+          files << { old_file: line[4..-1], chunks: [] }
         elsif line.start_with?('@@')
-          current_file[:chunks] << current_chunk if current_chunk
           old_line, old_length, new_line, new_length = line.split(' ')[1..2].flat_map { |p| p.tr('-+', '').split(',').map(&:to_i) }
-          current_chunk = { old_start: old_line, old_length: old_length, new_start: new_line, new_length: new_length, lines: [] }
+          files.last[:chunks] << { old_start: old_line, old_length: old_length, new_start: new_line, new_length: new_length, lines: [] }
         elsif line.start_with?(' ')
-          old_line += 1 if current_chunk[:lines].any?
-          new_line += 1 if current_chunk[:lines].any?
-          current_chunk[:lines] << [old_line, new_line]
+          old_line += 1 if files.last[:chunks].last[:lines].any?
+          new_line += 1 if files.last[:chunks].last[:lines].any?
+          files.last[:chunks].last[:lines] << [old_line, new_line]
         elsif line.start_with?('+')
-          new_line += 1 if current_chunk[:lines].any?
-          current_chunk[:lines] << [nil, new_line]
+          new_line += 1 if files.last[:chunks].last[:lines].any?
+          files.last[:chunks].last[:lines] << [nil, new_line]
         elsif line.start_with?('-')
-          old_line += 1 if current_chunk[:lines].any?
-          current_chunk[:lines] << [old_line, nil]
+          old_line += 1 if files.last[:chunks].last[:lines].any?
+          files.last[:chunks].last[:lines] << [old_line, nil]
         end
       end
-      current_file[:chunks] << current_chunk if current_chunk
-      files << current_file if current_file
-      files
     end
   end
 end
