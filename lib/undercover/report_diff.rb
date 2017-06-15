5c3fe18388713af365186710ebe25ef6e56f8855
Nathan Griffith <nathan.griffith@betterment.com>
2017-06-15 16:37:31 -0400

    Memoize expensive work

--- lib/undercover/report_diff.rb
+++ lib/undercover/report_diff.rb
@@ -19,22 +19,22 @@ module Undercover
     attr_accessor :left, :right, :renamed_files
 
     def files_by_filename
-      filenames.each_with_object({}) do |name, hsh|
+      @files_by_filename ||= filenames.each_with_object({}) do |name, hsh|
         hsh[name] = FileDiff.new(left_files_by_filename[name]&.first,
                                  right_files_by_filename[name]&.first)
       end
     end
 
     def left_files_by_filename
-      left.files.group_by { |file| renamed_files[file.filename] || file.filename }
+      @left_files_by_filename ||= left.files.group_by { |file| renamed_files[file.filename] || file.filename }
     end
 
     def right_files_by_filename
-      right.files.group_by(&:filename)
+      @right_files_by_filename ||= right.files.group_by(&:filename)
     end
 
     def filenames
-      left_files_by_filename.keys | right_files_by_filename.keys
+      @filenames ||= left_files_by_filename.keys | right_files_by_filename.keys
     end
   end
 end
