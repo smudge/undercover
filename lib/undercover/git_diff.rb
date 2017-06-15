f4c2ca15b3aa1cf1af81d348d8ad7f54835344d9
Nathan Griffith <nathan.griffith@betterment.com>
2017-06-15 13:21:46 -0400

    Switch to OpenStruct instead of hashes

--- lib/undercover/git_diff.rb
+++ lib/undercover/git_diff.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 module Undercover
   class GitDiff
     attr_reader :files
@@ -15,22 +17,22 @@ module Undercover
       new_line = nil
       string.each_line.each_with_object([]) do |line, files|
         if line.start_with?('+++')
-          files.last[:new_file] = line[4..-1]
+          files.last.new_file = line[4..-1]
         elsif line.start_with?('---')
-          files << { old_file: line[4..-1], chunks: [] }
+          files << OpenStruct.new(old_file: line[4..-1], new_file: nil, chunks: [])
         elsif line.start_with?('@@')
           old_line, old_length, new_line, new_length = line.split(' ')[1..2].flat_map { |p| p.tr('-+', '').split(',').map(&:to_i) }
-          files.last[:chunks] << { old_start: old_line, old_length: old_length, new_start: new_line, new_length: new_length, lines: [] }
+          files.last.chunks << OpenStruct.new(old_start: old_line, old_length: old_length, new_start: new_line, new_length: new_length, lines: []) # rubocop:disable Metrics/LineLength
         elsif line.start_with?(' ')
-          old_line += 1 if files.last[:chunks].last[:lines].any?
-          new_line += 1 if files.last[:chunks].last[:lines].any?
-          files.last[:chunks].last[:lines] << [old_line, new_line]
+          old_line += 1 if files.last.chunks.last.lines.any?
+          new_line += 1 if files.last.chunks.last.lines.any?
+          files.last.chunks.last.lines << [old_line, new_line]
         elsif line.start_with?('+')
-          new_line += 1 if files.last[:chunks].last[:lines].any?
-          files.last[:chunks].last[:lines] << [nil, new_line]
+          new_line += 1 if files.last.chunks.last.lines.any?
+          files.last.chunks.last.lines << [nil, new_line]
         elsif line.start_with?('-')
-          old_line += 1 if files.last[:chunks].last[:lines].any?
-          files.last[:chunks].last[:lines] << [old_line, nil]
+          old_line += 1 if files.last.chunks.last.lines.any?
+          files.last.chunks.last.lines << [old_line, nil]
         end
       end
     end
--- spec/git_diff_spec.rb
+++ spec/git_diff_spec.rb
@@ -46,32 +46,32 @@ RSpec.describe Undercover::GitDiff do
     expect(subject.files.length).to eq 2
 
     subject.files.first.tap do |file|
-      expect(file[:chunks].count).to eq 1
-      file[:chunks].first.tap do |chunk|
-        expect(chunk[:old_start]).to eq 1
-        expect(chunk[:new_start]).to eq 1
-        expect(chunk[:old_length]).to eq 5
-        expect(chunk[:new_length]).to eq 5
-        expect(chunk[:lines]).to eq [[1, 1], [2, nil], [nil, 2], [3, 3], [4, 4], [5, 5]]
+      expect(file.chunks.count).to eq 1
+      file.chunks.first.tap do |chunk|
+        expect(chunk.old_start).to eq 1
+        expect(chunk.new_start).to eq 1
+        expect(chunk.old_length).to eq 5
+        expect(chunk.new_length).to eq 5
+        expect(chunk.lines).to eq [[1, 1], [2, nil], [nil, 2], [3, 3], [4, 4], [5, 5]]
       end
     end
 
     subject.files.last.tap do |file|
-      expect(file[:chunks].count).to eq 2
-      file[:chunks].first.tap do |chunk|
-        expect(chunk[:old_start]).to eq 6
-        expect(chunk[:new_start]).to eq 6
-        expect(chunk[:old_length]).to eq 10
-        expect(chunk[:new_length]).to eq 6
-        expect(chunk[:lines]).to eq [[6, 6], [7, 7], [8, nil], [9, nil], [10, nil], [11, nil], [12, 8], [13, 9], [14, 10]]
+      expect(file.chunks.count).to eq 2
+      file.chunks.first.tap do |chunk|
+        expect(chunk.old_start).to eq 6
+        expect(chunk.new_start).to eq 6
+        expect(chunk.old_length).to eq 10
+        expect(chunk.new_length).to eq 6
+        expect(chunk.lines).to eq [[6, 6], [7, 7], [8, nil], [9, nil], [10, nil], [11, nil], [12, 8], [13, 9], [14, 10]]
       end
 
-      file[:chunks].last.tap do |chunk|
-        expect(chunk[:old_start]).to eq 23
-        expect(chunk[:new_start]).to eq 19
-        expect(chunk[:old_length]).to eq 6
-        expect(chunk[:new_length]).to eq 7
-        expect(chunk[:lines]).to eq [[23, 19], [24, 20], [nil, 21], [25, 22], [26, 23]]
+      file.chunks.last.tap do |chunk|
+        expect(chunk.old_start).to eq 23
+        expect(chunk.new_start).to eq 19
+        expect(chunk.old_length).to eq 6
+        expect(chunk.new_length).to eq 7
+        expect(chunk.lines).to eq [[23, 19], [24, 20], [nil, 21], [25, 22], [26, 23]]
       end
     end
   end
